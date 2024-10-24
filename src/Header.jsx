baa283b01e27fe1510943222d9fe0cf9fb1dc63f
Raiga SASAYAMA <raiga0310brilio@gmail.com>
2024-10-25 08:53:33 +0900

    feat(WIP): Headerを追加

---
+++ src/Header.jsx
@@ -0,0 +1,8 @@
+export function Header() {
+    return (
+        <header className="header">
+            <h2 className="app__name">掲示板アプリ</h2>
+            <button className="new__thread">新規スレッド</button>
+        </header>
+    );
+}
