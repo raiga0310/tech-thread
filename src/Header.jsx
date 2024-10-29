c4f7a71dbb284ea2be1d200a65e081d08d7cb926
Raiga SASAYAMA <raiga0310brilio@gmail.com>
2024-10-29 15:49:25 +0900

    feat: 投稿フォーム作成

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 export function Header() {
+    const navigete = useNavigate();
     return (
         <header className="header">
-            <h2 className="app__name">掲示板アプリ</h2>
+            <h2 className="app__name" onClick={() => navigete('/')}>掲示板アプリ</h2>
             <a className="new__thread" href="/threads/new">新規スレッド</a>
         </header>
     );
--- src/ThreadDetail.jsx
+++ src/ThreadDetail.jsx
@@ -8,7 +8,10 @@ function ThreadDetail() {
     return (
         <>
             <Header />
-            <PostsList title={threadTitle}/>
+            <div className="posts__container">   
+                <PostsList title={threadTitle}/>
+                <NewPostForm />
+            </div>
         </>
     )
 }
@@ -43,5 +46,32 @@ function PostsList({ title }) {
 PostsList.propTypes = {
     title: PropTypes.string.isRequired,
 };
+
+function NewPostForm() {
+    const [post, setPost] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        await fetch('https://railway.bulletinboard.techtrain.dev/threads/${threadID}/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ post })
+        });
+
+        setPost('');
+    }
+
+    return (
+        <div className="form__thread">
+            <h3>投稿する</h3>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="post" id="name" onChange={(e) => setPost(e.target.value)} placeholder="投稿" />
+                <input type="submit" value="送信" />
+            </form>
+        </div>
+    )
+}
 
 export default ThreadDetail;
